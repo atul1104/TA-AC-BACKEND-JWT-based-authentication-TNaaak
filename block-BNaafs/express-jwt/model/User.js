903dfde191ca30179d15292b7e7c6cb72db5c28b
atul1104 <waghamareatul2@gmail.com>
2022-04-19 15:36:48 +0530

    assignment

---
+++ block-BNaafs/express-jwt/model/User.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, minlength: 5, required: true },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.password && this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.verifyPassword = async function (password) {
+  try {
+    var result = await bcrypt.compare(password, this.password);
+    return result;
+  } catch (error) {
+    return error;
+  }
+  bcrypt.compare();
+};
+
+userSchema.methods.signToken = async function () {
+  var payload = { userId: this.id, email: this.email };
+  try {
+    var token = await jwt.sign(payload, 'thisissecret');
+    return token;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = mongoose.model('User', userSchema);
